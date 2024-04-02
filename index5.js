3f3ac7d599987a17c8da7ba67de36b49e81700b9
nurbek2107 <abduaxatov002@gmail.com>
2024-04-02 22:11:27 +0500

    feat: date

--- index5.js
+++ index5.js
@@ -1,6 +1,6 @@
-let year = +prompt("Joriy yilni kiriting:")
-let moth = +prompt("Joriy oyni kiriting (1-12):")
-let data = +prompt("Joriy kunni kiriting:")
+let year = +prompt("yilni kiriting:")
+let moth = +prompt("oyni kiriting (1-12):")
+let data = +prompt("kunni kiriting:")
 function date(year,moth,data,){
     let a = new Date(year,moth - 1,data) 
     /* Date   mustaqil formatda vaqtning bir lahzasini ifodalaydi. */
@@ -14,4 +14,4 @@ date(year,moth,data)
 
 let yil = date(year,moth,data);
 
-console.log("1970 yilning 1-yanvardan boshlab " + yil + " kun o'tgan.");
+alert("1970 yilning 1-yanvardan boshlab " + yil + " kun o'tgan.");
