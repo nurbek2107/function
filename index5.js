89036ffc66d6c1a88a42d87d01cc282ed5a4f065
nurbek2107 <abduaxatov002@gmail.com>
2024-04-02 22:16:29 +0500

    date

--- index5.js
+++ index5.js
@@ -5,13 +5,8 @@ function date(year,moth,data,){
     let a = new Date(year,moth - 1,data) 
     /* Date   mustaqil formatda vaqtning bir lahzasini ifodalaydi. */
     let b = a.getTime() //millisekundlar sonini qaytaradi
-    let c = 1000 * 60 * 60 * 24;
-    let d = Math.floor(b / c); //eng katta butun sonni qaytaradi
+    let d = Math.floor(b / (1000 * 60 * 60 * 24)); //eng katta butun sonni qaytaradi
     return d
 }
-
 date(year,moth,data) 
-
-let yil = date(year,moth,data);
-
-alert("1970 yilning 1-yanvardan boshlab " + yil + " kun o'tgan.");
+alert("1970 yilning 1-yanvardan boshlab " + date + " kun o'tgan.");
